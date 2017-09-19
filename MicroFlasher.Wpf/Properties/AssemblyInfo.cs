eb9fab44e30434ef193a7d5bd63fa0550a59e071
Sergey Savchuk <savchuk.sergey@gmail.com>
2017-09-19 17:15:46 +0300

    new csproj

--- MicroFlasher.Wpf/Properties/AssemblyInfo.cs
+++ MicroFlasher.Wpf/Properties/AssemblyInfo.cs
@@ -5,12 +5,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MicroFlasher")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MicroFlasher")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
